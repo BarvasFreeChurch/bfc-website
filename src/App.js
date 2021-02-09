66f91bd17f81a67754e67a7e7dfde840f4c2f592
BarvasFreeChurch <78113428+BarvasFreeChurch@users.noreply.github.com>
2021-02-09 14:47:16 +0000

    update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload!
+          BARVAS FREE CHURCH website is being developed by Andrew Macleod
         </p>
         <a
           className="App-link"
